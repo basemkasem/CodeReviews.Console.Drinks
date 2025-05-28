d50a08cf46f5693571a29c176031419bdddade34
basemkasem <69323267+basemkasem@users.noreply.github.com>
2025-05-28 17:45:14 +0300

    create the basic models

---
+++ STUDY.ConsoleApp.Drinks/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace STUDY.ConsoleApp.Drinks;
+
+public record Category(
+    [property: JsonPropertyName("strCategory")]
+    string CategoryName);
+
+public record CategoryRoot
+{
+    [JsonPropertyName("drinks")] 
+    public List<Category> Categories { get; init; } = new();
+}
---
+++ STUDY.ConsoleApp.Drinks/Drink.cs
@@ -0,0 +1,12 @@
+﻿namespace STUDY.ConsoleApp.Drinks;
+
+public class Drink
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string ImageUrl { get; set; }
+    
+}
