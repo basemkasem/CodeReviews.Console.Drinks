ff063f70e4f94ac6002d754b42ad8111aedb06d7
basemkasem <69323267+basemkasem@users.noreply.github.com>
2025-05-28 17:45:42 +0300

    create GetAllCategories method

---
+++ STUDY.ConsoleApp.Drinks/DrinkService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace STUDY.ConsoleApp.Drinks;
+
+public class DrinkService
+{
+    private readonly HttpClient _httpClient = new();
+    public async Task<List<Category>> GetAllCategories()
+    {
+		string apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+        String responseBody = await response.Content.ReadAsStringAsync();
+
+        var categories = JsonSerializer.Deserialize<CategoryRoot>(responseBody) ?? new CategoryRoot();
+        return categories.Categories;
+    }
+}
